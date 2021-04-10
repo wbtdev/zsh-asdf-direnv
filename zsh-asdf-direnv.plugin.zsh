1b6a553799a7d6039bac95752c373f157b4da2e8
Brad Thorne <brad@wbt.dev>
2021-04-10 12:24:58 -0600

    chore: asdf completion

--- zsh-asdf-direnv.plugin.zsh
+++ zsh-asdf-direnv.plugin.zsh
@@ -1,24 +1,19 @@
 # Copyright (c) 2021 Brad Thorne
-# ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
+ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
 # ASDF_COMPLETIONS="$ASDF_DIR/completions"
 
 0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
 0=${${(M)0:#/*}:-$PWD/$0}
 
 # Then ${0:h} to get plugin's directory
-
-# if [[ ${zsh_loaded_plugins[-1]} != */zsh-asdf-direnv && -z ${fpath[(r)${0:h}]} ]] {
-#     fpath+=( "${0:h}" )
-# }
+if [[ ${zsh_loaded_plugins[-1]} != */zsh-asdf-direnv && -z ${fpath[(r)${0:h}]} ]] {
+    fpath+=( "${0:h}" )
+}
 
 typeset -gA Plugins
 Plugins[ZSH_ASDF_DIRENV_DIR]="${0:h}"
 
-# # Load command
-# [ -f "$ASDF_DIR/asdf.sh" ] && {
-#   . "$ASDF_DIR/asdf.sh";
-#   # [ -d "$ASDF_COMPLETIONS" ] && fpath+=("$ASDF_COMPLETIONS")
-# }
+source "$ASDF_DIR/asdf.sh" || return
 
 [[ -n ${commands[direnv]} ]] || return
 
